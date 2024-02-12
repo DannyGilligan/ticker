5de15682e7860a9591d493e0dcae11a259f93317
DannyGilligan <gilligan.danny@gmail.com>
2024-02-12 00:06:59 +0000

    Install django money library

--- trades/models.py
+++ trades/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Trade(models.Model):
+    trader = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="ticker_trade"
+    )
+    ticker = models.CharField(max_length=5)
+    
+
