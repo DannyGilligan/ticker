604f422566cc4b8f2c1cb4620e74ab3be89a20cd
DannyGilligan <gilligan.danny@gmail.com>
2024-02-13 20:31:15 +0000

    Add fields to forms.py

--- trades/forms.py
+++ trades/forms.py
@@ -2,4 +2,47 @@ from django import forms
 from .models import Trade
 
 class TradeDetailsForm(forms.ModelForm):
-    
+    class Meta:
+        model = Trade
+        formFields = ['trader', 'ticker', 'date_opened', 'trade_amount', 'opening_price', 'position', 'units_transacted', 'trade_status', 'broker', 'closing_price', 'date_closed', 'trade_duration', 'realised_pl', 'result']
+        formLabels = {
+            'trader': 'Trader',
+            'ticker': 'Ticker',
+            'date_opened': 'Date opened',
+            'trade_amount': 'Trade amount',
+            'opening_price': 'Opening price',
+            'position': 'Position',
+            'trade_status': 'Trade status',
+            'broker': 'Broker',
+            'closing_price': 'Closing price',
+            'date_closed': 'Date closed'
+        }
+        widgets = {
+            'trader': forms.TextInput(attrs={'class': 'form-control'}),
+
+            'ticker': forms.Charfield(
+                widget=forms.Select(choices=STOCK_CHOICES),
+                attrs={'class': 'form-control'}), 
+
+            'date_opened': forms.DateField(attrs={'class': 'form-control'}),
+
+            'trade_amount': forms.TextInput(attrs={'class': 'form-control'}),
+
+            'opening_price': forms.NumberInput(attrs={'class': 'form-control'}),
+
+            'position': forms.Charfield(
+                widget=forms.Select(choices=POSITION_CHOICES),
+                attrs={'class': 'form-control'}),
+
+            'trade_status': forms.Charfield(
+                widget=forms.Select(choices=STATUS_CHOICES),
+                attrs={'class': 'form-control'}),
+
+            'broker': forms.Charfield(
+                widget=forms.Select(choices=BROKER_CHOICES),
+                attrs={'class': 'form-control'}),
+
+            'closing_price': forms.NumberInput(attrs={'class': 'form-control'}),
+
+            'date_closed': forms.DateField(attrs={'class': 'form-control'}),
+        }
--- trades/views.py
+++ trades/views.py
@@ -4,38 +4,50 @@ from django.http import HttpResponse
 from django.urls import reverse
 from django.shortcuts import get_object_or_404
 from .models import Trade
+from .forms import TradeDetailsForm
 
 # Create your views here.
 def index(request):
     return render(request, 'trades/index.html')
 
+
 def all_trades(request):
+    """
+    The 'trades' variable below holds all the data in the
+    'Trade' model, this is the context that allows
+    the all_trades.html template to display the
+    requested information to the user when rendered.
+    """
     return render(request, 'trades/all_trades.html', {
-        # 'trades' variable holds all the data in the
-        # Trade model, this is the context that allows
-        # the all_trades.html template to display the
-        # requested information to the user when rendered.
         'trades': Trade.objects.all()
     })
 
+
 def open_trades(request):
+    """
+    The 'open_trades' variable below holds all the data in the
+    'Trade' model filtered by 'OPEN' status, this is the context that allows
+    the open_trades.html template to display the
+    requested information to the user when rendered.
+    """
     return render(request, 'trades/open_trades.html', {
-        # 'trades' variable holds all the data in the
-        # Trade model, this is the context that allows
-        # the all_trades.html template to display the
-        # requested information to the user when rendered.
         'open_trades': Trade.objects.filter(trade_status="OPEN")
     })
 
+
 def closed_trades(request):
+    """
+    The 'closed_trades' below variable holds all the data in the
+    'Trade' model filtered by 'CLOSED' status, this is the context that allows
+    the closed_trades.html template to display the
+    requested information to the user when rendered.
+    """
     return render(request, 'trades/closed_trades.html', {
-        # 'trades' variable holds all the data in the
-        # Trade model, this is the context that allows
-        # the all_trades.html template to display the
-        # requested information to the user when rendered.
         'closed_trades': Trade.objects.filter(trade_status="CLOSED")
     })
+
 
 def view_trade(request, id):
     trade = Trade.objects.get(pk=id)
     return HttpeResponseDirect(reverse('index'))
+
