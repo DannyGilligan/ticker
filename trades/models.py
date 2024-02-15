7c54d30bfb7217356a0b8cc05cabedb65d85c80e
DannyGilligan <gilligan.danny@gmail.com>
2024-02-15 21:04:54 +0000

    Fix pep8 errors in models.py

--- trades/models.py
+++ trades/models.py
@@ -6,13 +6,19 @@ from djmoney.money import Money
 from djmoney.models.validators import MaxMoneyValidator, MinMoneyValidator
 
 
-
 # Create your models here.
 
-POSITION_CHOICES = (('BUY','BUY'), ('SELL','SELL'))
-STATUS_CHOICES = (('OPEN','OPEN'), ('CLOSED','CLOSED'))
-STOCK_CHOICES = (('TSLA','TSLA'),('UBER','UBER'),('AAPL','AAPL'),('AMZN','AMZN'),('AMD','AMD'),('RIOT','RIOT'),('NVDA','NVDA'),('NFLX','NFLX'),('SNAP','SNAP'),('META','META'))
-BROKER_CHOICES = (('ETORO','ETORO'), ('ROBINHOOD','ROBINHOOD'), ('IG','IG'), ('DEGIRO','DEGIRO'), ('INTERACTIVE BROKERS','INTERACTIVE BROKERS'), ('PLUS500','PLUS500'))
+POSITION_CHOICES = (('BUY', 'BUY'), ('SELL', 'SELL'))
+STATUS_CHOICES = (('OPEN', 'OPEN'), ('CLOSED', 'CLOSED'))
+STOCK_CHOICES = (('TSLA', 'TSLA'), ('UBER', 'UBER'), ('AAPL', 'AAPL'),
+                 ('AMZN', 'AMZN'), ('AMD', 'AMD'), ('RIOT', 'RIOT'),
+                 ('NVDA', 'NVDA'), ('NFLX', 'NFLX'), ('SNAP', 'SNAP'),
+                 ('META', 'META'))
+BROKER_CHOICES = (('ETORO', 'ETORO'), ('ROBINHOOD', 'ROBINHOOD'), ('IG', 'IG'),
+                  ('DEGIRO', 'DEGIRO'),
+                  ('INTERACTIVE BROKERS', 'INTERACTIVE BROKERS'),
+                  ('PLUS500', 'PLUS500'))
+
 
 class Trade(models.Model):
 
@@ -22,39 +28,49 @@ class Trade(models.Model):
 
     ticker = models.CharField(
         max_length=5,
-        choices = STOCK_CHOICES
+        choices=STOCK_CHOICES
     )
 
     date_opened = models.DateField()
-    
-    trade_amount = MoneyField(max_digits=10, decimal_places=2, default_currency='USD', validators=[MinMoneyValidator(1),])
-    
-    opening_price = MoneyField(max_digits=10, decimal_places=2, default_currency='USD', validators=[MinMoneyValidator(1),])
-    
+
+    trade_amount = MoneyField(max_digits=10,
+                              decimal_places=2,
+                              default_currency='USD',
+                              validators=[MinMoneyValidator(1), ])
+
+    opening_price = MoneyField(max_digits=10,
+                               decimal_places=2,
+                               default_currency='USD',
+                               validators=[MinMoneyValidator(1), ])
+
     position = models.CharField(
-        max_length = 4,
-        choices = POSITION_CHOICES,
-        default = 'BUY'
+        max_length=4,
+        choices=POSITION_CHOICES,
+        default='BUY'
     )
 
     trade_status = models.CharField(
-        max_length = 6,
-        choices = STATUS_CHOICES,
-        default = 'OPEN'
+        max_length=6,
+        choices=STATUS_CHOICES,
+        default='OPEN'
     )
 
     broker = models.CharField(
-        max_length = 20,
-        choices = BROKER_CHOICES
+        max_length=20,
+        choices=BROKER_CHOICES
     )
 
-    closing_price = MoneyField(max_digits=10, decimal_places=2, default_currency='USD', blank=True, null=True, validators=[MinMoneyValidator(1),])
+    closing_price = MoneyField(max_digits=10,
+                               decimal_places=2,
+                               default_currency='USD',
+                               blank=True, null=True,
+                               validators=[MinMoneyValidator(1), ])
 
     date_closed = models.DateField(blank=True, null=True)
 
     @property
     def trade_duration(self):
-        if self.date_closed == None:
+        if self.date_closed is None:
             return "PENDING"
         else:
             trade_duration = self.date_closed - self.date_opened
@@ -67,10 +83,11 @@ class Trade(models.Model):
 
     @property
     def realised_pl(self):
-        if self.closing_price == None:
+        if self.closing_price is None:
             return "PENDING"
         else:
-            realised_pl = (self.units_transacted * self.closing_price) - self.trade_amount
+            realised_pl = (self.units_transacted *
+                           self.closing_price) - self.trade_amount
             return realised_pl
 
     @property
@@ -88,11 +105,11 @@ class Trade(models.Model):
 
     class Meta:
         ordering = ["date_opened"]
-    
 
     def __str__(self):
-        return f'{self.date_opened} {self.position} {self.ticker} {self.trade_amount}'
-    
+        return f'{self.date_opened} {self.position} \
+            {self.ticker} {self.trade_amount}'
+   
 
 
 
